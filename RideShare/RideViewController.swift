bea1515fe2e0508ddbe9bd422541d94f36db48d2
NYUTerminal <praveen.mareedu07@gmail.com>
2015-04-28 00:41:53 -0400

    maps code commented

--- RideShare/RideViewController.swift
+++ RideShare/RideViewController.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 
-class RideViewController: UIViewController , GMSMapViewDelegate {
+class RideViewController: UIViewController /*, GMSMapViewDelegate */{
     
     override func viewDidLoad() {
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
-        
-        var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+//        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+//            longitude: 151.20, zoom: 6)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        self.view = mapView
+//        
+//        var marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
         // Do any additional setup after loading the view, typically from a nib.
     }
     
