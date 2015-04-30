dce50cc97dc63d08dd38aef0e1cbfe5e9b9cf17b
AlexMathew06 <alexmathew0910@gmail.com>
2015-04-30 02:59:14 -0400

    Accepted offers scene functionality

--- RideShare/RideShare/AcceptedOffersCell.swift
+++ RideShare/RideShare/AcceptedOffersCell.swift
@@ -26,7 +26,7 @@ class AcceptedOffersCell: UITableViewCell {
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
-        // Configure the view for the selected state
+        
     }
     
 }
