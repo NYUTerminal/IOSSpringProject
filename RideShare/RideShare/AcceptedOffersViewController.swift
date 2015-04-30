f41c78b53720c2c2078c6a07fa07c8f34a75fdb3
AlexMathew06 <alexmathew0910@gmail.com>
2015-04-30 03:01:16 -0400

    Accepted offers scene functionality

--- RideShare/RideShare/AcceptedOffersViewController.swift
+++ RideShare/RideShare/AcceptedOffersViewController.swift
@@ -45,7 +45,6 @@ class AcceptedOffersViewController: UIViewController, UITableViewDelegate , UITa
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Table view data source
