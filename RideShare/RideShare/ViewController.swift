1a3e03faae9fd21a30c1911a10f97b14564b507d
NYUTerminal <praveen.mareedu07@gmail.com>
2015-04-29 23:39:45 -0400

    changes to date picker and time picjer

--- RideShare/RideShare/ViewController.swift
+++ RideShare/RideShare/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, GMSMapViewDelegate {
+class ViewController: UIViewController, GMSMapViewDelegate , UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var source: UITextField!
 
+    @IBOutlet weak var destination: UITextField!
+    
+    @IBOutlet weak var date: UITextField!
+    
+    @IBOutlet weak var time: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        time.delegate=self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,43 @@ class ViewController: UIViewController, GMSMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    
+    func getDatePicker(){
+        date.resignFirstResponder()
+        DatePickerDialog().show(title: "Pick Date", doneButtonTitle: "Done", cancelButtonTitle: "Cancel", datePickerMode: .Date) {
+            (date) -> Void in
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "MM-dd-yyyy"
+            self.date.text = dateFormatter.stringFromDate(date)
+            self.date.text = "\(date)"
+        }
+    }
+    
+    func getTimePicker(){
+        time.resignFirstResponder()
+        DatePickerDialog().show(title: "Pick Time", doneButtonTitle: "Done", cancelButtonTitle: "Cancel", datePickerMode: .Time) {
+            (time) -> Void in
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "HH:mm"
+            self.time.text = dateFormatter.stringFromDate(time)
+            //self.time.text = "\(time)"
+        }
+    }
+    
+    @IBAction func getDate(sender: UITextField) {
+        getDatePicker()
+    }
+    
+    
+    @IBAction func getTime(sender: UITextField) {
+        getTimePicker()
+    }
     
     func getCurrentLocation(){
         
